ea11403acdf20ba152d8c6ba6dbc716163449a15
Nikki Kononov <nikitakononov93@gmail.com>
2016-11-27 11:35:50 +0200

    Some minor chanegs for the demo

--- src/app/main/dashboard/task-overview/task-overview.component.ts
+++ src/app/main/dashboard/task-overview/task-overview.component.ts
@@ -8,12 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TaskOverviewComponent implements OnInit {
 
   tasks: { taskNum: number, name: string, status: string, risk: string, tag: string }[] = [
-    {taskNum: 1, name: 'Wash the deck', status: 'Pending', risk: 'Low', tag: 'cleaning' },
-    {taskNum: 2, name: 'Unload the cargo', status: 'Not Started', risk: 'High', tag: 'cargo' },
-    {taskNum: 3, name: 'Board the ship', status: 'Done', risk: 'Medium', tag: 'boarding' },
-    {taskNum: 4, name: 'Load the Cargo', status: 'Not Started', risk: 'High', tag: 'cargo' },
-    {taskNum: 5, name: 'System Check', status: 'Not Started', risk: 'Low', tag: 'online' }
-  ]
-  cnstructor() {}
+    {taskNum: 1, name: 'Cargo Unloading', status: 'Ongoing', risk: 'Medium', tag: 'cleaning'}
+  ];
+
   ngOnInit() {}
 }
